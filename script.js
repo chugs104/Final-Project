a9a27a8813743e51a3d4c163d777ead5822232f0
chugs104 <44503196+chugs104@users.noreply.github.com>
2019-04-29 13:56:03 -0400

    Update script.js
    Fixed variables, added query selector

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var a = aInput;
-var b = bInput;
-var c = cInput;
+var a = document.querySelector('#aInput');
+var b = document.querySelector('#bInput');
+var c = document.querySelector('#cInput');
+
+button.addEventListener('click', runFunction.solveEquation);
     
-function solveClicked(a, b, c) {
+function solveEquation(a, b, c) {
     var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
     var result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
     return result + "<br>" + result2;
