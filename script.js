6956cc25a4f4874cdff6fdc00b30d95b6a4e3916
chugs104 <44503196+chugs104@users.noreply.github.com>
2019-04-29 12:48:26 -0400

    Update script.js
    Added basic function to work quadratic equation.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+function solve(a, b, c) {
+    var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    var result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    return result + "<br>" + result2;
+}
+document.write(solve(1, 1, -1));
