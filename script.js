44c51851870ccd1e9f6f342a2803b15b7f8ca02c
chugs104 <44503196+chugs104@users.noreply.github.com>
2019-04-29 13:46:09 -0400

    Update script.js
    Tried to make variables and button function work.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var a = 
-var b =
-var c = 
+var a = aInput;
+var b = bInput;
+var c = cInput;
     
-function solve(a, b, c) {
+function solveClicked(a, b, c) {
     var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
     var result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
     return result + "<br>" + result2;
 }
-
