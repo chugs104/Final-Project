93db65a72e88c5d3ef44d009c3a936c2844b392a
chugs104 <44503196+chugs104@users.noreply.github.com>
2019-05-06 09:09:30 -0400

    Update script.js
    Fixed some issues with the button and text inputs

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+const button = document.querySelector('#solve');
 var a = document.querySelector('#aInput');
 var b = document.querySelector('#bInput');
 var c = document.querySelector('#cInput');
+var result1;
+var result2;
 
-button.addEventListener('click', runFunction.solveEquation);
-    
-function solveEquation(a, b, c) {
-    var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-    var result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-    return result + "<br>" + result2;
+function solveEquation() {
+  a = parseFloat(a);
+  b = parseFloat(b);
+  c = parseFloat(c);
+    result1 = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    document.querySelector(".x1").innerHTML = result1;
+    document.querySelector(".x2").innerHTML = result2;
 }
+
+button.addEventListener('click', solveEquation, "false");
