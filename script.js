77ed8dcddbda6e4f7242260ddba27b8a4bae7394
chugs104 <44503196+chugs104@users.noreply.github.com>
2019-05-13 12:31:49 -0400

    Update script.js
    Added new function for the calculation of the vertex points.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const button = document.querySelector('#solve');
-var a = document.querySelector('#aInput').value;
-var b = document.querySelector('#bInput').value;
-var c = document.querySelector('#cInput').value;
+const button2 = document.querySelector('#reset');
 var result1;
 var result2;
+var xVertex;
+var yVertex;
 
 function solveEquation() {
   var a = document.querySelector('#aInput').value;
@@ -30,3 +30,29 @@ function solveEquation() {
 
 button.addEventListener('click', solveEquation, "false");
 
+button2.addEventListener ('click', resetVariable, "false")
+
+function resetVariable() {
+  document.querySelector(".x1").innerHTML = ""
+  document.querySelector(".x2").innerHTML = ""
+  document.querySelector("#aInput").value = ""
+  document.querySelector("#bInput").value = ""
+  document.querySelector("#cInput").value = ""
+}
+
+function vertexPoints() {
+  xVertex = (-b / (2 * a));
+  yVertex = (a * ((-b / (2 * a))(-b / (2 * a))) + b * (-b / (2 * a)) + c)
+  if (document.querySelector(".x1").innerHTML === "NaN") {
+    document.querySelector(".").innerHTML = "No value"
+  }
+  else {
+    document.querySelector(".xVertexValue").innerHTML = xVertex.toFixed(5);
+  }
+  if (document.querySelector(".x2").innerHTML === "NaN") {
+    document.querySelector(".").innerHTML = "No value"
+  }
+  else {
+    document.querySelector(".yVertexValue").innerHTML = yVertex.toFixed(5);
+}
+}
